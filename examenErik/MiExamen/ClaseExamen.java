81b3c35f356fb7c33fef95044c76630523bc74a3
erikruafernandez <159418513+erikruafernandez@users.noreply.github.com>
2024-03-18 13:08:23 +0100

    Añadidos comentarios de documentacion en claseExamen

--- examenErik/MiExamen/ClaseExamen.java
+++ examenErik/MiExamen/ClaseExamen.java
@@ -1,5 +1,10 @@
 package miexamen;
-
+/**
+ * 
+ * @author Erik
+ * @version 1.0.0
+ * @since 18/03/2024
+ */
 public class ClaseExamen {
 
 
@@ -7,3 +12,21 @@ public class ClaseExamen {
         System.out.println("Estoy haciendo el examen de la unidad de la UT3");    }
     
 }
+
+/**
+*<!DOCTYPE html>
+*<html lang="en">
+*<head>
+*    <meta charset="UTF-8">
+*   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+*    <meta name="viewport" content="width=, initial-scale=1.0">
+*    <title>Entornos de desarrollo UT3</title>
+*    
+*</head>
+*<body>
+*   <h1>Entornos de Desarrollo UT3</h1>
+*   <p>Esta clase sirve para realizar el exame de la UT3 del modulo de Entornos de Desarrollo</p>
+*   <a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU" target="_blank">Curso de Git y GitHub de Píldoras Informáticas</a>
+*</body>
+*</html>
+* /
