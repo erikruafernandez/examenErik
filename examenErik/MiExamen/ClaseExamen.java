31fbce3611284da5b4122d0db96e8082f07d959e
erikruafernandez <159418513+erikruafernandez@users.noreply.github.com>
2024-03-18 12:47:26 +0100

    Añadimos los primeros ficheros al proyecto

---
+++ examenErik/MiExamen/ClaseExamen.java
@@ -0,0 +1,9 @@
+package miexamen;
+
+public class ClaseExamen {
+
+
+    public static void main(String[] args) {
+        System.out.println("Estoy haciendo el examen de la unidad de la UT3");    }
+    
+}
